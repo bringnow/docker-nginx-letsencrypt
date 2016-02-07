70aa03acc06c28461ba5d7c48064e356f7229c3d
Fabian Köster <koesterreich@fastmail.fm>
2016-02-07 21:17:28 +0100

    Create directory if not existant

--- prepare_dhparam.sh
+++ prepare_dhparam.sh
@@ -1,6 +1,12 @@
 # /bin/bash
 
 if [ ! -f /etc/nginx/ssl/dhparam.pem ]; then
+
+    if [ ! -d /etc/nginx/ssl ]; then
+      mkdir -p /etc/nginx/ssl
+    fi
+
     echo "dhparam file /etc/nginx/ssl/dhparam.pem does not exist. Generating one with 4086 bit. This will take a while..."
     openssl dhparam -out /etc/nginx/ssl/dhparam.pem 4096
+    echo "Finished. Starting nginx now..."
 fi
