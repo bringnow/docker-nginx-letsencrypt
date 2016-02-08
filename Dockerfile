ad6f4f0e68a6797ac832dd4bf82e7dfd306422b6
Fabian Köster <koesterreich@fastmail.fm>
2016-02-08 01:10:50 +0100

    Add volume for dhparam

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,6 @@ COPY letsencryptauth.conf /etc/nginx/snippets/letsencryptauth.conf
 COPY sslconfig.conf /etc/nginx/snippets/sslconfig.conf
 COPY prepare_dhparam.sh /usr/local/bin/
 
+VOLUME /etc/nginx/dhparam
+
 CMD /usr/local/bin/prepare_dhparam.sh && nginx -g "daemon off;"
--- prepare_dhparam.sh
+++ prepare_dhparam.sh
@@ -1,12 +1,8 @@
 # /bin/bash
 
-if [ ! -f /etc/nginx/ssl/dhparam.pem ]; then
+if [ ! -f /etc/nginx/dhparam/dhparam.pem ]; then
 
-    if [ ! -d /etc/nginx/ssl ]; then
-      mkdir -p /etc/nginx/ssl
-    fi
-
-    echo "dhparam file /etc/nginx/ssl/dhparam.pem does not exist. Generating one with 4086 bit. This will take a while..."
-    openssl dhparam -out /etc/nginx/ssl/dhparam.pem 4096
+    echo "dhparam file /etc/nginx/dhparam/dhparam.pem does not exist. Generating one with 4086 bit. This will take a while..."
+    openssl dhparam -out /etc/nginx/dhparam/dhparam.pem 4096
     echo "Finished. Starting nginx now..."
 fi
